622670717bc52aadbb888259a2a4fd4cd5b11995
issacsamalex <issacsamalex@gmail.com>
2023-12-31 12:59:57 +0530

    initial commit

---
+++ config/dbConnection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+        console.log('Connected to mongoDB')
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = connectDB;
