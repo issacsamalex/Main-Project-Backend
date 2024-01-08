9f7378820f36d2d5146891c14aa44fd1652edbe8
Vaishnav S <109717500+vaishnavskd@users.noreply.github.com>
2024-01-08 21:25:24 +0530

    updated .env file and also db connected

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
-
-
+require('dotenv').config()
+const URI=process.env.URI
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        });
-        console.log('Connected to mongoDB')
+        await mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+        console.log('Connected to MongoDB')
     } catch (error) {
         console.log(error);
     }
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+require('dotenv').config()
+const PORT=process.env.PORT
+const connectDB=require('./config/dbConnection')
+connectDB()
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
